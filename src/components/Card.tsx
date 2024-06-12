2f69e24365f1f77e2ae544507fdc2b2244230c5c
facundom <facundomiglierini@gmail.com>
2024-06-12 14:43:22 -0300

    set max width for cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,7 +9,7 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ title, description, image }) => {
   return (
-    <div className="card flex flex-row md:flex-col bg-gray-300 md:px-10 md:py-8 rounded-[20px] mx-3 my-3">
+    <div className="card flex flex-row md:flex-col bg-gray-300 md:px-10 md:py-8 rounded-[20px] mx-3 my-3 md:max-w-[300px]">
       {image && (
         <div className="md:w-1/4">
           <img src={image} alt={title} className="card-image md:pb-3 min-w-[50px]" />
