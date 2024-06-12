ca0e135c03e8c44b21abfeb6c8ec63ec7a042b3d
facundom <facundomiglierini@gmail.com>
2024-06-12 14:40:08 -0300

    finished selection page

--- src/app/selection/page.tsx
+++ src/app/selection/page.tsx
@@ -3,16 +3,17 @@ import Card from '@/components/Card'
 import { poppins } from '../fonts'
 
 export default function Selection() {
+
   return (
     <>
       <Header />
       <main className='flex h-screen flex-col items-center justify-center py-8 px-4 sm:p-24 lg:p-32 gap-2 sm:gap-4 lg:gap-8'>
         <h1
-          className={`${poppins.className} text-center text-6xl font-medium`}
+          className={`${poppins.className} text-center text-3xl md:text-6xl font-medium`}
         >
-        Código HTML a mejorar
+       Código HTML a mejorar
         </h1>
-        <p className='text-center md:text-xl xl:mb-12'>
+        <p className='text-left mx-3 mt-5 md:text-center md:text-xl xl:mb-12'>
           El código que será accesible puede cargarse de múltiples maneras.
         </p>
         <div className='flex flex-col md:flex-row'>
