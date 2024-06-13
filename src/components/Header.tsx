873a5099be68d4c97ba9367d7f7b1b4582ef7a72
fots <facundotomatis@gmail.com>
2024-06-13 20:54:52 -0300

    autogenerate navbar, changed to spanish

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,9 +12,9 @@ import {
 
 export const Header = () => {
   const menuItems = {
-    Selection: '/selection',
-    Community: '/community',
-    Configuration: '/config',
+    Selección: '/selection',
+    Comunidad: '/community',
+    Configuración: '/config',
   }
 
   return (
@@ -25,38 +25,44 @@ export const Header = () => {
 
       <NavbarContent className='sm:hidden pr-3' justify='center'>
         <NavbarBrand>
-          <Link href='/' className={`${poppins.className} text-black text-xl font-bold`}>
-            HTML <span className="ml-1" style={{ color: '#E44D25' }}>AI</span>ccesible
+          <Link
+            href='/'
+            className={`${poppins.className} text-black text-xl font-bold`}
+          >
+            HTML{' '}
+            <span className='ml-1' style={{ color: '#E44D25' }}>
+              AI
+            </span>
+            ccesible
           </Link>
         </NavbarBrand>
       </NavbarContent>
 
       <NavbarContent className='hidden sm:flex gap-4' justify='end'>
         <NavbarBrand>
-          <Link href='/' className={`${poppins.className} text-black text-2xl font-bold`}>
-            HTML <span className="ml-2" style={{ color: '#E44D25' }}>AI</span>ccesible
+          <Link
+            href='/'
+            className={`${poppins.className} text-black text-2xl font-bold`}
+          >
+            HTML{' '}
+            <span className='ml-2' style={{ color: '#E44D25' }}>
+              AI
+            </span>
+            ccesible
           </Link>
         </NavbarBrand>
-        <NavbarItem>
-          <Link color='foreground' href='selection'>
-            Selection
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color='foreground' href='community'>
-            Community
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color='foreground' href='config'>
-            Configuration
-          </Link>
-        </NavbarItem>
+        {[...Object.entries(menuItems)].map(([key, value], index) => (
+          <NavbarItem key={`${key}-${index}-nav`}>
+            <Link color='foreground' href={value}>
+              {key}
+            </Link>
+          </NavbarItem>
+        ))}
       </NavbarContent>
 
       <NavbarMenu>
         {[...Object.entries(menuItems)].map(([key, value], index) => (
-          <NavbarMenuItem key={`${key}-${index}`}>
+          <NavbarMenuItem key={`${key}-${index}-menu`}>
             <Link className='w-full' href={value} size='lg'>
               {key}
             </Link>
