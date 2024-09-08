1c19a78beb7b9ef5d0deba2d5d39e3c8dbcfa1a3
facundom <facundomiglierini@gmail.com>
2024-09-08 16:26:59 -0300

    Added translations for metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,24 @@
 import './globals.css'
 import 'react-toastify/dist/ReactToastify.min.css';
-import type { Metadata } from 'next'
-import { NextUIProvider } from '@nextui-org/react'
+import { NextUIProvider, Spinner } from '@nextui-org/react'
 import { roboto } from './fonts'
 import Providers from './context/Provider'
 import { Suspense } from 'react';
 import {NextIntlClientProvider} from 'next-intl';
+import {getTranslations} from 'next-intl/server';
 import {getLocale, getMessages} from 'next-intl/server';
 import RouteGuard from '@/components/RouteGuard'
 
-
-export const metadata: Metadata = {
-  title: 'HTML AIccesible',
-  description:
-    'Es una herramienta de inteligencia artificial que convierte automáticamente contenido HTML en formatos accesibles, siguiendo las pautas de accesibilidad web (WCAG).',
+ 
+export async function generateMetadata() {
+  const t = await getTranslations('Layout');
+ 
+  return {
+    title: 'HTML AIccesible',
+    description: t("description"),
+  };
 }
+
 
 export default async function RootLayout({
   children,
@@ -27,7 +31,7 @@ export default async function RootLayout({
   return (
     <html lang={locale}>
       <body className={`${roboto.className} flex flex-col h-screen font-size-text-adjust-base`}>
-        <Suspense fallback={null}>
+        <Suspense fallback={<Spinner/>}>
           <NextIntlClientProvider messages={messages}>
             <NextUIProvider>
               <Providers>
